3b5bb25d1c738082db09363326f87052efa3a4b6
Prekksu <novrialsandi@gmail.com>
2023-03-04 15:16:29 +0700

    push

--- Module 1/Day 4/index.js
+++ Module 1/Day 4/index.js
@@ -1,19 +1,36 @@
 // Create a function that can create a triangle pattern according to the height we provide like the
 // following :
-
-let num = 1;
-for (let i = 0; i < 4; i++) {
-  let a = "";
-  for (let j = 0; j <= i; j++) {
-    a  ,10+= "0" + num + " ";
-    num++;
+function triangle(num) {
+  for (let i = 0; i < 4; i++) {
+    let a = "";
+    for (let j = 0; j <= i; j++) {
+      if (num < 10) {
+        a += "0" + num + " ";
+      } else a += num + " ";
+      num++;
+    }
+    console.log(a);
   }
-  console.log(`${a}`);
 }
+triangle(1);
 
 // // Create a function that can loop the number of times according to the input we provide, and will
 // replace multiples of 3 with "Fizz", multiples of 5 with "Buzz", multiples of 3 and 5 with "FizzBuzz".
 // Parameters : n → total looping
+
+function loop(n) {
+  for (let i = 1; i <= n; i++)
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+      console.log("Fizz");
+    } else if (i % 5 == 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+}
+loop(15);
 
 // Create a function to calculate Body Mass Index (BMI)
 // Formula : BMI = weight (kg) / (height (meter))²
@@ -25,19 +42,27 @@ for (let i = 0; i < 4; i++) {
 // 30.0 - 39.9 return “very overweight”
 // > 39.9 return “obesity”
 
-let w = 75;
-let h = 178;
 function BMI(w, h) {
   BMI = w / h ** 2;
   if (BMI < 18.5) {
-    console.log("less weight");
-  } else if (BMI >= 18.5) {
-    console.log("ideal");
-  } else if (BMI > 24.9) {
-    console.log("overweight");
-  } else if (BMI > 29.9) {
-    console.log("very overweiht");
-  } else if (BMI >= 40) {
-    console.log("obesity");
+    return "less weight";
+  } else if (BMI >= 18.5 && BMI <= 24.9) {
+    return "ideal";
+  } else if (BMI >= 25 && BMI <= 29.9) {
+    return "overweight";
+  } else if (BMI >= 30 && BMI <= 39.9) {
+    return "very overweiht";
+  } else {
+    return "obesity";
   }
 }
+console.log(BMI(75, 1.78));
+
+// // Write a function to remove all odd numbers in an array and return a new array that contains even numbers only
+
+function odd(arr) {
+  arr = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+}
+
+// Write a function to split a string and convert it into an array of words
+// Example : “Hello World” → [“Hello”, “World”]
