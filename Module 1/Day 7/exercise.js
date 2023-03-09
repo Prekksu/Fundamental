7f6924500ee8cdd655a45d9d54b71bdd510c6e17
Prekksu <novrialsandi@gmail.com>
2023-03-09 10:17:46 +0700

    push

---
+++ Module 1/Day 7/exercise.js
@@ -0,0 +1,9 @@
+let arr1 = [
+  { name: " student 1", email: "student1@gmail.com" },
+  { name: "student 2", email: "student2@Gmail.com" },
+];
+let arr2 = [
+  { name: "Student 1", email: "student1@mail.com" },
+  { name: "Student 3", email: "student3@mail.com" },
+];
+
